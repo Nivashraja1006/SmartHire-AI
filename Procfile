437f1dc118f70510d7a9cf1745f9e0867209069b
Nivashraja1006 <nivasraja12@gmail.com>
2026-09-09 00:31:41 +0530

    Fix deployment config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn run:application -k eventlet -w 1
--- run.py
+++ run.py
@@ -1,15 +1,14 @@
-import os
 from app import create_app, socketio
 
 app = create_app()
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    debug = app.config.get('DEBUG', True)
+# This is REQUIRED for gunicorn
+application = app
+
+if __name__ == "__main__":
     socketio.run(
         app,
-        host='0.0.0.0',
-        port=port,
-        debug=debug,
-        allow_unsafe_werkzeug=True,
+        host="0.0.0.0",
+        port=5000,
+        debug=True
     )
